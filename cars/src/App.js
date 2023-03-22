3ff80da6c5c719eb8b29e7296064ecf5157d6796
Hoang Nguyen <hoangisnot@gmail.com>
2023-03-22 16:07:54 -0700

    Create the App
    deleted files that come with npm install and then create basically that need for the application

---
+++ cars/src/App.js
@@ -0,0 +1,15 @@
+import CarForm from './components/CarForm';
+import CarList from './components/CarList';
+import CarValue from './components/CarValue';
+import CarSearch from './components/CarSearch';
+
+function App() {
+  return <div>
+    <CarForm />
+    <CarList/>
+    <CarValue />
+    <CarSearch />
+  </div>
+}
+
+export default App
